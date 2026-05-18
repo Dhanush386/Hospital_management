e72c152ae6a6eabc50ec958844b02b94c3aab1fa
Dhanush <dhanush386@users.noreply.github.com>
2026-05-18 19:38:16 +0530

    Add Vercel configuration and static files build script

--- smart_hospital/settings.py
+++ smart_hospital/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -44,6 +45,15 @@ MIDDLEWARE = [
     'django_htmx.middleware.HtmxMiddleware',
     'accounts.middleware.RoleRedirectMiddleware',
 ]
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
 
 ROOT_URLCONF = 'smart_hospital.urls'
 
